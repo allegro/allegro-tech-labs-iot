19f22cde9b6463a207f4cbaa194108c67658d18f
Artur Maćkowiak <awmackowiak@gmail.com>
2018-04-12 12:04:53 +0200

    ASD

---
+++ src/switcher/switcher.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+"""switch-level
+
+Usage:
+  switch-level <stage_id>
+  switch-level (-h | --help)
+  switch-level (-v|--version)
+
+Options:
+  -h --help             Show this screen.
+  -v --version          Show version.
+"""
+import git
+from docopt import docopt
+
+
+def main():
+    args = docopt(__doc__, version='checker 0.1.0')
+    if args['<stage_id>']:
+        repo = git.Repo('.')
+        cmd = repo.git
+        cmd.reset('HEAD')
+        cmd.pull()
+        cmd.checkout('{}'.format(args['<stage_id>']))
+        repo.active_branch
+
+
+if __name__ == "__main__":
+    main()
